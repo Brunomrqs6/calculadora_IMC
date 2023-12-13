c95aba49179f7b106c9d6e1c6cb5f698dedafbe9
Bruno Marques <yatosad@gmail.com>
2023-12-13 09:40:38 -0300

    estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Formulario from "./components/Formulario"
-import Tabela from "./components/Tabela"
+
 
 function App() {
   return (
     <>
       <Formulario />
-      <Tabela />
     </>
     
   )
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -13,34 +13,89 @@ const Formulario = () => {
         setAltura(evento.target.value)
     }
 
-    const imc = () => {
+
+    const mostraResposta = () => {
+
         const calcula = parseFloat(peso / (altura ** 2))
         console.log(calcula)
 
-        if (calcula < 18.5) {
-            return <p>Seu IMC foi de {calcula.toFixed(2)}, está na classificação de <span className={styles.classificacao}>Magreza</span>.</p>
-        } else if (calcula > 18.5 && calcula < 24.9) {
-            return <p>Seu IMC foi de {calcula.toFixed(2)}, está na classificação <span className={styles.classificacao}>Normal.</span></p>
-        } else if (calcula > 25.0 && calcula < 29.9) {
-            return <p>Seu IMC foi de {calcula.toFixed(2)}, está na classificação de <span className={styles.classificacao}>Sobrepreso.</span></p>
-        } else if (calcula > 30.0 && calcula < 39.0) {
-            return <p>Seu IMC foi de {calcula.toFixed(2)}, está na classificação de <span className={styles.classificacao}>Obesidade.</span></p>
-        } else if (calcula > 40.0) {
-            return <p>Seu IMC foi de {calcula.toFixed(2)}, está na classificação de <span className={styles.classificacao}>Obesidade grave.</span></p>
+        console.log(altura)
+
+
+
+        if (calcula < 18.5 && altura > 1.0) {
+            return <p className={styles.resultado}>Seu IMC foi de {calcula.toFixed(2)}, está na classificação de <span className={styles.classificacao}>Magreza</span>.</p>
+        } else if (calcula > 18.5 && calcula < 24.9 && altura > 1.0) {
+            return <p className={styles.resultado}>Seu IMC foi de {calcula.toFixed(2)}, está na classificação <span className={styles.classificacao}>Normal.</span></p>
+        } else if (calcula > 25.0 && calcula < 29.9 && altura > 1.0) {
+            return <p className={styles.resultado}>Seu IMC foi de {calcula.toFixed(2)}, está na classificação de <span className={styles.classificacao}>Sobrepreso.</span></p>
+        } else if (calcula > 30.0 && calcula < 39.0 && altura > 1.0) {
+            return <p className={styles.resultado}>Seu IMC foi de {calcula.toFixed(2)}, está na classificação de <span className={styles.classificacao}>Obesidade.</span></p>
+        } else if (calcula > 40.0 && altura > 1.0) {
+            return <p className={styles.resultado}>Seu IMC foi de {calcula.toFixed(2)}, está na classificação de <span className={styles.classificacao}>Obesidade grave.</span></p>
         }
     }
 
 
-    
+
 
     return (
         <div className="container">
-            <form>
-                <input type="number" placeholder="Peso" onChange={alteraPeso}/>
-                <input type="number" step={0.01} placeholder="Altura" onChange={alteraAltura}/>
-                <button type="submit">Calcular</button>
-                {imc()}
+            <form className={styles.form}>
+                <input className={styles.input} type="number" placeholder="Peso" onChange={alteraPeso} />
+                <input className={styles.input} type="number" step={0.01} placeholder="Altura" onChange={alteraAltura} />
             </form>
+            {mostraResposta()}
+            <table className={styles.tabela} align="center" >
+                <thead>
+                    <tr>
+                        <th>IMC</th>
+                        <th>Classificação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className={styles.corpoTabela}>
+                            MENOR QUE 18,5
+                        </td>
+                        <td className={styles.corpoTabela}>
+                            MAGREZA
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className={styles.corpoTabela}>
+                            ENTRE 18,5 E 24,9
+                        </td>
+                        <td className={styles.corpoTabela}>
+                            NORMAL
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className={styles.corpoTabela}>
+                            ENTRE 25,0 E 29,9
+                        </td>
+                        <td className={styles.corpoTabela}>
+                            SOBREPESO
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className={styles.corpoTabela}>
+                            ENTRE 30,0 E 39,9
+                        </td>
+                        <td className={styles.corpoTabela}>
+                            OBESIDADE
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className={styles.corpoTabela}>
+                            MAIOR QUE 40,0
+                        </td>
+                        <td className={styles.corpoTabela}>
+                            OBESIDADE GRAVE
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
